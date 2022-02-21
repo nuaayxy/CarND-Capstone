20ec273cbc4ec7406da721b9aaea0fa358fea32e
xye <xye@g.clemson.edu>
2022-02-21 00:01:24 -0800

    typo fix

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -95,7 +95,7 @@ class DBWNode(object):
             # if <dbw is enabled>:
             #   self.publish(throttle, brake, steer)
             
-            if not None in (self.current_vel, self.linear_vel, self.curr_ang_vel):
+            if not None in (self.current_vel, self.linear_vel, self.angular_vel):
                 self.throttle, self.brake, self.steering = self.controller.control(self.current_vel,
                                                                                    self.dbw_enabled,
                                                                                    self.linear_vel,
